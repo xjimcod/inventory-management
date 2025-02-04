4fa76bc11facd3add2bad82f1120c79633676e98
Jimy Jonathan <jim.llr01@gmail.com>
2025-02-04 11:42:43 -0500

    edstock name changed to urosur

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -83,7 +83,7 @@ const Sidebar = () => {
       >
         <Image
           src="https://s3-inventorymanagement-xjim.s3.us-east-1.amazonaws.com/logo.png"
-          alt="edstock-logo"
+          alt="urosur-logo"
           width={27}
           height={27}
           className="rounded w-8"
@@ -93,7 +93,7 @@ const Sidebar = () => {
             isSidebarCollapsed ? "hidden" : "block"
           } font-extrabold text-2xl`}
         >
-          EDSTOCK
+          UROSUR
         </h1>
 
         <button
@@ -146,7 +146,7 @@ const Sidebar = () => {
 
       {/* FOOTER */}
       <div className={`${isSidebarCollapsed ? "hidden" : "block"} mb-10`}>
-        <p className="text-center text-xs text-gray-500">&copy; 2024 Edstock</p>
+        <p className="text-center text-xs text-gray-500">&copy; 2025 UROSUR</p>
       </div>
     </div>
   );
